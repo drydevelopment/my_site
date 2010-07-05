256fc65a84448667cc972f90aebf66b43a8048a8
Doug Yoder <doug@drydevelopment.com>
2010-07-05 16:23:42 -0700

    remove obsolete migration

--- db/migrate/20100110173430_create_comments.rb
+++ db/migrate/20100110173430_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration
   def self.up
-    create_table :comments do |t|
-	  t.integer :post_id, :null => false
-	  t.integer :user_id
+      create_table :comments do |t|
+      t.integer :post_id, :null => false
+      t.integer :user_id
       t.string :title, :limit => 50, :default => "" 
       t.text :comment, :default => "" 
       t.timestamps
