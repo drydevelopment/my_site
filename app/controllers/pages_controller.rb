d78cee9f17d2eb498af1b440a9e45c4ba1ad7bf0
Doug Yoder <doug@drydevelopment.com>
2010-07-05 19:09:10 -0700

    moved static pages to pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
-class HomeController < ApplicationController
-  helper [HomeHelper, PostsHelper]
-  def index
+class PagesController < ApplicationController
+  def about
+    @page_title = 'About'
+  end
+  
+  def home
     @portfolios = Portfolio.find(:all, :conditions => { :is_featured => 1 }, :limit => 3)
     @posts = Post.published(5)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,19 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :about
   map.resources :posts, :as => 'blog', :member => { :add_comment => :post }
-  
   map.resources :messages, :as => :contact, :collection => { :thank_you => :get }
   map.resources :users
-  map.resource :session
   map.resources :portfolio_pictures
   map.resources :portfolios, :as => 'work'
   
-  # map.contact '/contact', :controller => :messages, :action => 'new'
-  # map.connect 'contact/thank-you', :controller => :messages, :action => 'thank_you'
-
-  map.login '/login', :controller => 'sessions', :action => 'new'
-  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-
-  map.root :controller => "home"
-
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  map.resource :session
+  
+  map.with_options :controller => 'pages' do |pages|
+    pages.about 'about', :action => 'about'    
+    pages.root :action => 'home'
+  end
+  
+  map.with_options :controller => 'sessions' do |sessions|
+    sessions.login 'login', :action => 'new'
+    sessions.logout 'logout', :action => 'destroy'
+  end
 end
